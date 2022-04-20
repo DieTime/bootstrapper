ee0658fba95da1f9b34bede96e8781276782e48e
Denis Glazkov <d.glazkov@omp.ru>
2022-04-20 20:47:16 +0300

    [bootstrapper] Request sudo session for remember password

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import glob
 import sys
+import subprocess
 from typing import Final, List
 from config import Config
-from termutils import red, user_agree
+from termutils import underline, gray, red, green, user_agree
 
 DEFAULT_CONFIGS_DIR: Final = 'configs'
 
@@ -11,6 +12,18 @@ DEFAULT_CONFIGS_DIR: Final = 'configs'
 def get_config_paths(configs_dir: str) -> List[str]:
     search_query = os.path.join(configs_dir, "**/*.yaml")
     return glob.glob(search_query, recursive=True)
+
+
+def request_sudo_session() -> bool:
+    print(f'{gray("[!]")} Please login to {underline("sudo")} session')
+
+    call = subprocess.run(['sudo', 'echo', '-n', '""'], capture_output=True)
+    if call.returncode != 0:
+        print(f'{red("[!]")} Failed to login to {underline("sudo")} session {red("Bad")}')
+        return False
+
+    print(f'{green("[!]")} Successfully login to {underline("sudo")} session {green("Ok")}')
+    return True
 
 
 if __name__ == '__main__':
@@ -23,3 +36,11 @@ if __name__ == '__main__':
         sys.exit(f'{red("[!]")} {error}')
 
     accepted_configs = [cfg for cfg in configs if user_agree(cfg.get_question())]
+
+    if not request_sudo_session():
+        sys.exit(1)
+
+    print('-------------------------')
+
+    for config in accepted_configs:
+        config.process()
