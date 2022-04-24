b7dc14141b9a88f625006f7cf126bcd9458a9654
Denis Glazkov <d.glazkov@omp.ru>
2022-04-25 01:08:49 +0300

    [bootstrapper] Refator start script

--- clean-start.sh
+++ clean-start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # setup virtual environment
-sudo apt install python3-pip python3-venv -y
+sudo apt install -qq python3-pip python3-venv -y
 python3 -m venv env
 source ./env/bin/activate
 
 # install dependencies
-pip3 install -r requirements.txt
+pip3 install -q -r requirements.txt
 
 # run bootstrapper
-python3 main.py
+clear && python3 main.py
 
 # deactivate virtual environment
 deactivate
