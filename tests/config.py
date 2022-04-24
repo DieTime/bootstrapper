b15b566e3f3031865387ef830c4b21348b2031e1
Denis Glazkov <d.glazkov@omp.ru>
2022-04-24 23:13:01 +0300

    [bootstrapper] Move tests to special directory

--- tests/config.py
+++ tests/config.py
@@ -1,49 +1,54 @@
 import unittest
+import os
+import sys
+
+parent = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, parent)
 from config import Config, DEFAULT_PRIORITY
 
 
 class TestConfig(unittest.TestCase):
     def test_not_found(self):
         with self.assertRaises(RuntimeError):
-            _ = Config('test-configs/not-found.yaml')
+            _ = Config('tests/assets/not-found.yaml')
 
     def test_invalid_syntax(self):
         with self.assertRaises(RuntimeError):
-            _ = Config('test-configs/invalid-syntax.yaml')
+            _ = Config('tests/assets/invalid-syntax.yaml')
 
     def test_no_question(self):
         with self.assertRaises(RuntimeError):
-            _ = Config('test-configs/no-question.yaml')
+            _ = Config('tests/assets/no-question.yaml')
 
     def test_empty_question(self):
         with self.assertRaises(RuntimeError):
-            _ = Config('test-configs/empty-question.yaml')
+            _ = Config('tests/assets/empty-question.yaml')
 
     def test_no_priotiry(self):
-        cfg = Config('test-configs/no-priority.yaml')
+        cfg = Config('tests/assets/no-priority.yaml')
         self.assertEqual(cfg.get_priority(), DEFAULT_PRIORITY)
 
     def test_empty_priotiry(self):
         with self.assertRaises(RuntimeError):
-            _ = Config('test-configs/empty-priority.yaml')
+            _ = Config('tests/assets/empty-priority.yaml')
 
     def test_no_steps(self):
         with self.assertRaises(RuntimeError):
-            _ = Config('test-configs/no-steps.yaml')
+            _ = Config('tests/assets/no-steps.yaml')
 
     def test_empty_steps(self):
         with self.assertRaises(RuntimeError):
-            _ = Config('test-configs/empty-steps.yaml')
+            _ = Config('tests/assets/empty-steps.yaml')
 
     def test_valid(self):
-        config_path = 'test-configs/valid.yaml'
+        config_path = 'tests/assets/valid.yaml'
         config = Config(config_path)
 
         self.assertEqual(config.get_priority(), 1)
         self.assertEqual(config.get_name(), 'valid.yaml')
         self.assertEqual(config.get_path(), config_path)
         self.assertEqual(config.get_question(), 'Test config file?')
-        self.assertEqual(config.get_dir(), 'test-configs')
+        self.assertEqual(config.get_dir(), 'tests/assets')
         self.assertListEqual(config.get_steps(), [
             {'Check invalid syntax': ['...', '...']},
             {'Check question section exists': ['...', '...']},
